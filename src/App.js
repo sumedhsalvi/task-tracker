928c5efba403717c052f9d9ead694cddffc81cee
Sumedh Salvi <sumedhsalvi25@gmail.com>
2022-09-29 13:34:52 -0400

    Optional message if no tasks

--- src/App.js
+++ src/App.js
@@ -32,7 +32,11 @@ function App() {
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : ""}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
